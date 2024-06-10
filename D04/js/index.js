5b829fc39ea67127a957296dd5dd323b4238706b
AyelenAlcon <153888001+AyelenAlcon@users.noreply.github.com>
2024-06-09 21:58:25 -0300

    Desafio 4

---
+++ D04/js/index.js
@@ -0,0 +1,5 @@
+function sumarProductos(precioUnitario, cantidadDeseada){
+    return precioUnitario * cantidadDeseada
+}
+
+console.log('El total gastado en el producto es de $' + sumarProductos(1000,5))
